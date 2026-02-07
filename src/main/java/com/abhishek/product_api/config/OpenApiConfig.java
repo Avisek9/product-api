ea338e71e3d20da587502f51d4734fbe9ebd0e34
abhishek <abhisheksahu5395@gmail.com>
2026-02-07 18:40:50 +0530

    Remove test file and finalize Swagger setup

---
+++ src/main/java/com/abhishek/product_api/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.abhishek.product_api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI productApiOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Product API")
+                        .description("REST API for managing products")
+                        .version("1.0"));
+    }
+}
